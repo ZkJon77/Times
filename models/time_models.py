46579fd6e737d3d12b326186d1a725b4ba60316f
jonatan <extremeshow046@gmail.com>
2025-11-03 10:55:24 -0300

    Add files via upload

--- models/time_models.py
+++ models/time_models.py
@@ -2,16 +2,16 @@
 from db import db  
 
 # Define a classe Carro que representa a tabela 'carros' no banco de dados
-class jogo(db.Model):  
+class time(db.Model):  
     # Define o nome da tabela no banco de dados
-    __tablename__ = 'jogo'  
+    __tablename__ = 'time'  
 
     # Define as colunas da tabela 'carros'
     id = db.Column(db.Integer, primary_key=True)  # Coluna para o ID do jogo, chave primária
     titulo = db.Column(db.String(80), nullable=False)  # Coluna para o titulo do jogo, não pode ser nulo
-    genero = db.Column(db.String(80), nullable=False)  # Coluna para o genero do jogo, não pode ser nulo
-    desenvolvedor = db.Column(db.String(80), nullable=False)  # Coluna para o desenvolvedor do jogo, não pode ser nulo
-    plataforma = db.Column(db.String(80), nullable=False)  # Coluna para a plataforma do jogo, não pode ser nulo
+    liga = db.Column(db.String(80), nullable=False)  # Coluna para o genero do jogo, não pode ser nulo
+    jogadores = db.Column(db.String(80), nullable=False)  # Coluna para o desenvolvedor do jogo, não pode ser nulo
+    clube = db.Column(db.String(80), nullable=False)  # Coluna para a plataforma do jogo, não pode ser nulo
 
     # Método para retornar os dados do jogo
     #  como um dicionário
@@ -19,7 +19,7 @@ class jogo(db.Model):
         return {
             'id': self.id,  # ID do jogo
             'titulo': self.titulo,  # titulo do jogo
-            'genero': self.genero,  # genero do jogo
-            'desenvolvedor': self.desenvolvedor,  # desenvolvedor do jogo
-            'plataforma': self.plataforma  # Plataforma do jogo
+            'liga': self.liga,  # genero do jogo
+            'jogadores': self.jogadores,  # desenvolvedor do jogo
+            'clube': self.clube  # Plataforma do jogo
         }
